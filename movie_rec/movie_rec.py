1874eb843f217058ef6994e002c2f07c6484fbab
nick_tidwell <nicktidwell52@gmail.com>
2023-04-13 10:19:33 -0400

    merge projects

---
+++ movie_rec/movie_rec.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+from surprise import Dataset, SVD
+from surprise.model_selection import cross_validate
+from surprise import Reader
+from surprise import KNNWithMeans
+
+ratings_pd = pd.read_csv("data/ratings_small.csv")
+reader = Reader(rating_scale=(1, 5))
+
+# Load the movielens-100k dataset (download it if needed),
+# data = Dataset.load_builtin("ml-100k")
+data = Dataset.load_from_df(ratings_pd[['userId', 'movieId', 'rating']], reader)
+# We'll use the famous SVD algorithm.
+
+
+def find_best_k():
+    user_out = []
+    item_out = []
+
+    for i in range(1, 100):
+
+        #USER COSINE TEST
+        user_cosine = KNNWithMeans(k=i, sim_options={'name': 'cosine', 'user_based': True})
+        res = cross_validate(user_cosine, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+        user_out.append(["user_cosine",np.mean(res['test_rmse']), np.mean(res['test_mae']), i])
+        #USER MSD TEST
+        user_msd = KNNWithMeans(k=i, sim_options={'name': 'msd', 'user_based': True})
+        res = cross_validate(user_msd, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+        user_out.append(["user_msd",np.mean(res['test_rmse']), np.mean(res['test_mae']), i])
+        #USER PEARSON TEST
+        user_pearson = KNNWithMeans(k=i, sim_options={'name': 'pearson', 'user_based': True})
+        res = cross_validate(user_pearson, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+        user_out.append(["user_pearson",np.mean(res['test_rmse']), np.mean(res['test_mae']), i])
+
+        #ITEM COSINE TEST
+        item_cosine = KNNWithMeans(k=i, sim_options={'name': 'cosine', 'user_based': False})
+        res = cross_validate(item_cosine, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+        item_out.append(["item_cosine",np.mean(res['test_rmse']), np.mean(res['test_mae']), i])
+        #ITEM MSD TEST
+        item_msd = KNNWithMeans(k=i, sim_options={'name': 'msd', 'user_based': False})
+        res = cross_validate(item_msd, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+        item_out.append(["item_msd",np.mean(res['test_rmse']), np.mean(res['test_mae']), i])
+        #ITEM PEARSON TEST
+        item_pearson = KNNWithMeans(k=i, sim_options={'name': 'pearson', 'user_based': False})
+        res = cross_validate(item_pearson, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+        item_out.append(["item_pearson",np.mean(res['test_rmse']), np.mean(res['test_mae']), i])
+
+    user_out_pd = pd.DataFrame(user_out, columns=["Algo_Name", "RMSE", "MAE", "K"])
+    user_out_pd.to_csv("./output/user_out.csv")
+    item_out_pd = pd.DataFrame(item_out, columns=["Algo_Name", "RMSE", "MAE", "K"])
+    item_out_pd.to_csv("./output/item_out.csv")
+
+def evaluate_all_sims():
+    # Run 5-fold cross-validation and print results
+    pmf = SVD(biased=False)
+
+    user_cosine = KNNWithMeans(k=50, sim_options={'name': 'cosine', 'user_based': True})
+    user_msd = KNNWithMeans(k=50, sim_options={'name': 'msd', 'user_based': True})
+    user_pearson = KNNWithMeans(k=50, sim_options={'name': 'pearson', 'user_based': True})
+
+    item_cosine = KNNWithMeans(k=50, sim_options={'name': 'cosine', 'user_based': False})
+    item_msd = KNNWithMeans(k=50, sim_options={'name': 'msd', 'user_based': False})
+    item_pearson = KNNWithMeans(k=50, sim_options={'name': 'pearson', 'user_based': False})
+
+
+    output = []
+    res = cross_validate(pmf, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+    output.append(["PMF",np.mean(res['test_rmse']), np.mean(res['test_mae'])])
+
+    res = cross_validate(user_cosine, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+    output.append(["user_cosine",np.mean(res['test_rmse']), np.mean(res['test_mae'])])
+    res = cross_validate(user_msd, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+    output.append(["user_msd", np.mean(res["test_rmse"]), np.mean(res["test_mae"])])
+    res = cross_validate(user_pearson, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+    output.append(["user_pearson", np.mean(res["test_rmse"]), np.mean(res["test_mae"])])
+
+    res = cross_validate(item_cosine, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+    output.append(["item_cosine", np.mean(res["test_rmse"]), np.mean(res["test_mae"])])
+    res = cross_validate(item_msd, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+    output.append(["item_msd", np.mean(res["test_rmse"]), np.mean(res["test_mae"])])
+    res = cross_validate(item_pearson, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+    output.append(["item_pearson", np.mean(res["test_rmse"]), np.mean(res["test_mae"])])
+
+    out_fd = pd.DataFrame(output, columns=["Algo_Name", "RMSE", "MAE"])
+    out_fd.to_csv("./output/results.csv")
---
+++ movie_rec/output.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def filter_df(df, metric, func):
+    # print(df.loc[(df['metric'] == metric) ])
+    return df.loc[(df['metric'] == metric) ][func].to_list()
+
+def plot_bars():
+    results_pd = pd.read_csv("./output/res_colab.csv").sort_values(by=['metric'])
+    print(results_pd)
+    rmse = results_pd['RMSE']
+    mae = results_pd['MAE']
+    label = results_pd[['metric', 'type']].apply(lambda row: ' - '.join(row.values.astype(str)), axis=1)
+    # fig = plt.figure(figsize = (10, 5))
+
+
+    X = ['User', 'Item']
+    X_axis = np.arange(len(X))
+
+    fig, ax = plt.subplots(2)
+
+    ax[0].set_xticks(X_axis, X)
+    ax[0].bar(X_axis - 0.1,  filter_df(results_pd, 'cosine', 'RMSE') , width = 0.2, label = 'cosine')
+    ax[0].bar(X_axis +  0.1,  filter_df(results_pd, 'msd', 'RMSE') , width = 0.2, label = 'msd')
+    ax[0].bar(X_axis +  0.3,  filter_df(results_pd, 'pearson', 'RMSE') , width = 0.2, label = 'pearson')
+    ax[0].set_ylabel("RMSE")
+
+    ax[1].set_xticks(X_axis, X)
+    ax[1].bar(X_axis - 0.1,  filter_df(results_pd, 'cosine', 'MAE') , width = 0.2, label = 'cosine')
+    ax[1].bar(X_axis +  0.1,  filter_df(results_pd, 'msd', 'MAE') , width = 0.2, label = 'msd')
+    ax[1].bar(X_axis +  0.3,  filter_df(results_pd, 'pearson', 'MAE') , width = 0.2, label = 'pearson')
+    ax[1].set_ylabel("MAE")
+
+    plt.legend()
+    plt.show()
+
+
+def plot_k():
+    item_pd = pd.read_csv("./output/item_out.csv")
+    user_pd = pd.read_csv("./output/user_out.csv")
+
+    rmse_i = item_pd.loc[item_pd['metric'] == 'cosine']['RMSE'].to_list()
+    mae_i = item_pd.loc[item_pd['metric'] == 'cosine']['MAE'].to_list()
+    rmse_u = user_pd.loc[user_pd['metric'] == 'cosine']['RMSE'].to_list()
+    mae_u = user_pd.loc[user_pd['metric'] == 'cosine']['MAE'].to_list()
+    k = item_pd.loc[item_pd['metric'] == 'cosine']['K'].to_list()
+
+    fig, ax = plt.subplots(2)
+    ax[0].set_title("Item Colab filtering")
+    ax[0].plot(k, rmse_i, label="RMSE")
+    ax[0].set_ylabel("Error")
+    ax[0].plot(k, mae_i, label="MAE")
+    # ax[0].set_ylabel("MAE")
+    ax[1].set_title("User Colab filtering")
+    ax[1].plot(k, rmse_u, label="RMSE")
+    ax[1].set_ylabel("Error")
+    ax[1].plot(k, mae_u, label="MAE")
+    # ax[1].set_ylabel("MAE")
+
+
+    plt.xlabel("K")
+    plt.legend()
+    plt.show()
+
+    print("ITEM RMSE MIN: ", np.argmin(rmse_i))
+    print("ITEM MAE MIN: ", np.argmin(mae_i))
+    print("USER RMSE MIN: ", np.argmin(rmse_u))
+    print("USER MAE MIN: ", np.argmin(mae_u))
+plot_k()
